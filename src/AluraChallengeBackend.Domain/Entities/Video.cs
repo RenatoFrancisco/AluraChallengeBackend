25ce856ba27e572d8bff206a4e47e1202f0e45b2
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-07-30 22:33:37 -0300

    Creating the Project AluraChallengeBackend.Core

---
+++ src/AluraChallengeBackend.Domain/Entities/Video.cs
@@ -0,0 +1,22 @@
+﻿using AluraChallengeBackend.Core.DomainObjects;
+
+namespace AluraChallengeBackend.Domain.Entities
+{
+    public class Video : Entity
+    {
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public string Url { get; private set; }
+
+        public Video(string titulo, 
+                     string descricao, 
+                     string url)
+        {
+            Titulo = titulo;
+            Descricao = descricao;
+            Url = url;
+        }
+
+        public override string ToString() => $"{Titulo} - {Descricao} - {Url}";
+    }
+}
